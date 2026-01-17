401e97a4590394656e7419a2e12ed0e2a60c608b
zxmeng98 <meng007@e.ntu.edu.sg>
2026-01-17 19:28:46 +0800

    update AE

--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@ torchrun --nproc_per_node 4 \
     --node_rank 0 \
     --master_addr "192.168.1.40" \
     --master_port 2923 \
-    resgnn_pp_trainonall.py --dataset $dataset --num_layers $layers --hidden_channels $hidden --dropout 0.5 --lr 0.01 --bs 54025 --mb_size 54025
+    revgnn_pp_trainonall_sync_lm_reorder.py --dataset $dataset --num_layers $layers --hidden_channels $hidden --dropout 0.5 --lr 0.01 --bs 54025 --mb_size 54025
 
 torchrun --nproc_per_node 4 \
     --nnodes 2 \
     --node_rank 1 \
     --master_addr "192.168.1.40" \
     --master_port 2923 \
-    resgnn_pp_trainonall.py --dataset $dataset --num_layers $layers --hidden_channels $hidden --dropout 0.5 --lr 0.01 --bs 54025 --mb_size 54025
+    revgnn_pp_trainonall_sync_lm_reorder.py --dataset $dataset --num_layers $layers --hidden_channels $hidden --dropout 0.5 --lr 0.01 --bs 54025 --mb_size 54025
